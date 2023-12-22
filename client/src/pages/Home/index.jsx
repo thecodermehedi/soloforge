d9a343cdc5cbda14fc7a426319905cc949552ac4
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-22 19:39:58 +0600

    Add Banner component to Home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,10 +1,11 @@
+import Banner from "../../components/Home/Banner";
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <main>
+      <Banner />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
