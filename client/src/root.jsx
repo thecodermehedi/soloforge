5ffcd2952e952d9c12ef2f1e37e08ac88734aa75
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-22 19:38:55 +0600

    Refactor root component to include additional providers and components

--- client/src/root.jsx
+++ client/src/root.jsx
@@ -1,11 +1,20 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import {HelmetProvider} from "react-helmet-async";
 import {RouterProvider} from "react-router-dom";
-import "./index.css";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Toaster} from "react-hot-toast";
+import AuthProvider from "./providers/AuthProvider";
 import router from "./routes";
+import "./index.css";
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <HelmetProvider>
+  <AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <Toaster />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </AuthProvider>
+</HelmetProvider>
 );
