242169834e762c7cd0d17c7fbc078639ef61e90d
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-23 17:06:48 +0600

    Refactor useTask hook to use optional chaining and arrow function

--- client/src/hooks/useTask.jsx
+++ client/src/hooks/useTask.jsx
@@ -3,12 +3,11 @@ import {createTask, getTasks} from "../api/tasks";
 import useAuth from "./useAuth";
 const useTask = () => {
   const {user} = useAuth();
-  const userMail = user?.email;
   const queryClient = new QueryClient();
   const {data: tasks} = useQuery({
-    enabled: !!userMail,
+    enabled: !!user?.email,
     queryKey: ["tasks"],
-    queryFn: async (userMail) => await getTasks(userMail),
+    queryFn: async () => await getTasks(user.email),
   });
 
   const {mutateAsync: createTaskFn} = useMutation({
