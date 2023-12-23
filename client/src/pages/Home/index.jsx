7ebcbbc86c186a52060b245c842986e93bdeda1f
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-23 17:06:52 +0600

    Add components to Home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
 import Banner from "../../components/Home/Banner";
+import Benefit from "../../components/Home/Benefit";
+import CallToAction from "../../components/Home/CallToAction";
+import Contact from "../../components/Home/Contact";
+import FeatureList from "../../components/Home/FeatureList";
+import Features from "../../components/Home/Features";
+import Newsletter from "../../components/Home/Newsletter";
 
 const Home = () => {
   return (
     <main>
       <Banner />
+      <Features />
+      <FeatureList />
+      <Benefit />
+      <CallToAction />
+      <Newsletter />
+      <Contact />
     </main>
   );
 };
