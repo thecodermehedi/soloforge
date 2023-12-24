1fc339d5a0e4aec7b482e63f18ac389f47f7f963
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-24 17:59:27 +0600

    Refactor useTask hook to use useQueryClient and add deleteTask and updateTaskStatus mutations

--- client/src/hooks/useTask.jsx
+++ client/src/hooks/useTask.jsx
@@ -1,15 +1,14 @@
-import {QueryClient, useMutation, useQuery} from "@tanstack/react-query";
-import {createTask, getTasks} from "../api/tasks";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+import {createTask, deleteTask, getTasks, updateTaskStatus} from "../api/tasks";
 import useAuth from "./useAuth";
 const useTask = () => {
   const {user} = useAuth();
-  console.log("user.email:", user.email); // Check the value of user.email
-  const queryClient = new QueryClient();
-  const {data: tasks = []} = useQuery({
+  const queryClient = useQueryClient();
+  const {data: tasks} = useQuery({
     enabled: !!user?.email,
     queryKey: ["tasks"],
     queryFn: async () => await getTasks(user.email),
-    onError: (error) => console.error("useQuery error:", error), // Log any errors
+    onError: (error) => console.error("useQuery error:", error),
   });
 
   const {mutateAsync: createTaskFn} = useMutation({
@@ -19,7 +18,32 @@ const useTask = () => {
     },
   });
 
-  return {tasks, createTaskFn};
+  const {mutateAsync: deleteTaskFn} = useMutation({
+    mutationFn: deleteTask,
+    onSuccess: () => {
+      queryClient.invalidateQueries("tasks");
+    },
+  });
+
+  const {mutateAsync: updateTaskStatusFn} = useMutation({
+    mutationFn: async ({id, status}) => await updateTaskStatus(id, status),
+    onSuccess: () => {
+      queryClient.invalidateQueries("tasks");
+    },
+  });
+
+  const todoTasks = tasks?.filter((task) => task.status === "todo");
+  const ongoingTasks = tasks?.filter((task) => task.status === "ongoing");
+  const completedTasks = tasks?.filter((task) => task.status === "completed");
+
+  return {
+    todoTasks,
+    ongoingTasks,
+    completedTasks,
+    createTaskFn,
+    deleteTaskFn,
+    updateTaskStatusFn,
+  };
 };
 
 export default useTask;
