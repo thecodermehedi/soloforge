576253d6a48ed57e7baf0a8a107325fb5657610e
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-24 12:33:13 +0600

    Add logging and error handling to useTask hook

--- client/src/hooks/useTask.jsx
+++ client/src/hooks/useTask.jsx
@@ -3,22 +3,23 @@ import {createTask, getTasks} from "../api/tasks";
 import useAuth from "./useAuth";
 const useTask = () => {
   const {user} = useAuth();
+  console.log("user.email:", user.email); // Check the value of user.email
   const queryClient = new QueryClient();
-  const {data: tasks} = useQuery({
+  const {data: tasks = []} = useQuery({
     enabled: !!user?.email,
     queryKey: ["tasks"],
     queryFn: async () => await getTasks(user.email),
+    onError: (error) => console.error("useQuery error:", error), // Log any errors
   });
 
   const {mutateAsync: createTaskFn} = useMutation({
     mutationFn: createTask,
     onSuccess: () => {
       queryClient.invalidateQueries("tasks");
-    }
+    },
   });
 
   return {tasks, createTaskFn};
-
 };
 
 export default useTask;
