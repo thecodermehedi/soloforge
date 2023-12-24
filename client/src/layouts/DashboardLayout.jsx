93670d334665acedd15237188ff8d6e140ace5b0
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-24 19:33:58 +0600

    Remove profile link from dashboard layout

--- client/src/layouts/DashboardLayout.jsx
+++ client/src/layouts/DashboardLayout.jsx
@@ -27,26 +27,6 @@ const DashboardLayout = () => {
       ),
     },
     {
-      name: "Profile",
-      path: "/dashboard/profile",
-      icon: (
-        <svg
-          className="size-5"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill="currentColor"
-            fillRule="evenodd"
-            d="M8 7a4 4 0 1 1 8 0a4 4 0 0 1-8 0m0 6a5 5 0 0 0-5 5a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3a5 5 0 0 0-5-5z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ),
-    },
-    {
       name: "Create Task",
       path: "/dashboard/create-task",
       icon: (
