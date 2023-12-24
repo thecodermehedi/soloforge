0981ad8add0259403edc15f9c69ddd72e5161ebb
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-24 18:54:34 +0600

    Update axiosSecure.js to use different API URLs based on environment

--- client/src/api/axiosSecure.js
+++ client/src/api/axiosSecure.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL:
+    import.meta.env.VITE_ENV === "production"
+      ? import.meta.env.VITE_API_URL_PROD
+      : import.meta.env.VITE_API_URL_DEV,
   withCredentials: true,
 });
+
+console.log(import.meta.env.VITE_ENV, import.meta.env.VITE_API_URL_PROD, import.meta.env.VITE_API_URL_DEV);
 
 export default axiosSecure;
