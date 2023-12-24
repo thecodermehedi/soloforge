4896df74fe9f4cb872fc58b378b31a7886466ca4
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-24 17:59:23 +0600

    Add task deletion functionality to TodoItem component

--- client/src/components/TodoItem.jsx
+++ client/src/components/TodoItem.jsx
@@ -1,10 +1,19 @@
-
+import {Trash2} from "react-feather";
+import useTask from "../hooks/useTask";
 const TodoItem = ({task}) => {
+  const {deleteTaskFn} = useTask();
+  const handleTaskDeletion = async (id) => {
+    await deleteTaskFn(id);
+  };
   return (
-    <div className="bg-gray-200 pl-4 py-2 my-2">
-      {task.title}
+    <div className="bg-gray-200 min-h-10 border border-black text-sm flex justify-between items-center">
+     <p className="break-words pl-2 overflow-hidden w-48">{task.title}</p>
+      <Trash2
+        onClick={() => handleTaskDeletion(task?._id)}
+        className="size-5 hover:text-red-500 cursor-pointer mr-3"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
