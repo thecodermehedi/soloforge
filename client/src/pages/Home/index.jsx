e14471761e72e94e05a7ed58eb09679fef68b4b4
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-25 08:08:10 +0600

    Add AOS animation library to Home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from "react";
 import Banner from "../../components/Home/Banner";
 import Benefit from "../../components/Home/Benefit";
 import CallToAction from "../../components/Home/CallToAction";
@@ -6,8 +7,13 @@ import FeatureList from "../../components/Home/FeatureList";
 import Features from "../../components/Home/Features";
 import Newsletter from "../../components/Home/Newsletter";
 import Testimonial from "../../components/Home/Testimonial";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <main>
       <Banner />
