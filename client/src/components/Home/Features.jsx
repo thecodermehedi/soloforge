65158689735804de4928af3a1095d4cc4e93e188
Mehedi Hasan <thecodermehedi@gmail.com>
2023-12-25 08:08:06 +0600

    Add fade-in animation to Features component

--- client/src/components/Home/Features.jsx
+++ client/src/components/Home/Features.jsx
@@ -4,7 +4,12 @@ const Features = () => {
   return (
     <section className="my-5">
       <Container className="gap-x-12 items-center justify-between overflow-hidden md:flex">
-        <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
+        <div
+          className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl"
+          data-aos="fade-up"
+          data-aos-delay="600"
+          data-aos-duration="1000"
+        >
           <h2 className="text-3xl font-bold  md:text-4xl lg:text-5xl">
             Experience the Soloforge platform&#39;s responsive design on any
             device.
@@ -14,7 +19,12 @@ const Features = () => {
             from your phone, tablet, or PC.
           </p>
         </div>
-        <div className="py-10 lg:py-16 lg:px-10">
+        <div
+          className="py-10 lg:py-16 lg:px-10"
+          data-aos="fade-down"
+          data-aos-delay="600"
+          data-aos-duration="1000"
+        >
           <img src="/responsive.svg" className="" alt="" />
         </div>
       </Container>
